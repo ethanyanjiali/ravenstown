c579aa34f4d6dc2d3ce4048150599463b04b9c60
Ethan Li <yanjia.li@snapchat.com>
2018-02-05 22:29:59 -0800

    add about page

--- src/common/containers/SideNavContainer/SideNavContainer.js
+++ src/common/containers/SideNavContainer/SideNavContainer.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Modal, Button } from 'antd';
 import * as web3Actions from '../../../web3/actions';
 import SideNavItem from '../../components/SideNavItem/SideNavItem';
 import './SideNavContainer.css';
 import ravenLogo from '../../../assets/logo.svg';
+import ethLogo from '../../../assets/eth-logo.png';
 
 const mapStateToProps = state => ({
   isWeb3Loaded: state.web3.isWeb3Loaded,
 });
 
 class SideNavContainer extends Component {
+
+  state = {
+    aboutModalVisible: false,
+  }
 
   handleClickNavItem = (navId) => {
     const { dispatch } = this.props;
@@ -23,8 +29,15 @@ class SideNavContainer extends Component {
     }
   }
 
+  handleClickAbout = () => {
+    this.setState({
+      aboutModalVisible: true,
+    });
+  }
+
   render() {
     const { isWeb3Loaded } = this.props;
+    const { aboutModalVisible } = this.state;
 
     return (
       <div className='sidenav-container'>
@@ -35,6 +48,24 @@ class SideNavContainer extends Component {
         </div>
         <SideNavItem label='Town Square' navId='square' onClick={ this.handleClickNavItem } />
         { isWeb3Loaded && <SideNavItem label='Log Out' navId='logout' onClick={ this.handleClickNavItem } /> }
+        <SideNavItem label='About' onClick={ this.handleClickAbout } />
+        <Modal
+          title='About Us'
+          visible={ aboutModalVisible }
+          footer={null}
+          closable={ false }
+        >
+          <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            }}
+          >
+            <a style={{ textAlign: 'center' }} href='https://www.ethereum.org/' target='_blank' rel='noopener noreferrer'><img style={{ width: '40%' }} src={ ethLogo } alt='ethLogo' /></a>
+            <p>This project is powered by ethereum. We aim to build a decentralized space for people to talk freely. If you have any interest, or questions about upcoming new features, please contact me via liyanjia92@gmail.com</p>
+            <Button onClick={ () => this.setState({ aboutModalVisible: false })}>OK</Button>
+          </div>
+        </Modal>
       </div>
     );
   }
